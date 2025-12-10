bc98db8fd0a60897e4c22d1301b10a36bf88d9ac
Swadesh Chatterjee <swadeshchatterjee512@gmail.com>
2025-12-10 11:12:07 +0530

    selection sort

--- src/Sorting/SelectionSort.java
+++ src/Sorting/SelectionSort.java
@@ -10,20 +10,17 @@ public class SelectionSort {
 
     }
     static void selectionSort(int[] arr) {
-        int n = arr.length;
-        for(int i = 0; i < n-1; i++){ // i represent the current index
-            //Find the minimum element in unsorted part of array
-            int min_index = i;
-            for(int j = i+1; j < n; j++){
-                if(arr[j] < arr[min_index]){
-                    min_index = j;
+        for (int i = 0; i < arr.length-1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
                 }
             }
-            //swap current element and minimum element -> current index i will have correct element
-            // a[min_index], a[i]
             int temp = arr[i];
-            arr[i] = arr[min_index];
-            arr[min_index] = temp;
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
         }
+
     }
 }
