478eb72b9b0d4c6367c0dcfb90a9a190ed12af50
Swadesh Chatterjee <swadeshchatterjee512@gmail.com>
2026-03-20 23:08:22 +0530

    complete two questions

---
+++ src/Arrays/FirstAndLastIndex.java
@@ -0,0 +1,30 @@
+package Arrays;
+
+import java.util.Arrays;
+
+public class FirstAndLastIndex {
+    public static void main(String[] args) {
+        int[] arr = {1,1,4,4,4,5,5,6,7,8};
+        System.out.println(Arrays.toString(findIndexes(arr, 4)));
+
+    }
+    static int findFirstIndex(int[] arr,int target){
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    static int[] findIndexes(int[] arr, int target) {
+        int first = -1, last = -1;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                if (first == -1) first = i;
+                last = i;
+            }
+        }
+        return new int[]{first, last};
+    }
+}
---
+++ src/Arrays/SumOfPairs.java
@@ -0,0 +1,15 @@
+package Arrays;
+
+public class SumOfPairs {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4};
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i+1; j < arr.length; j++) {
+                sum+=arr[i]+arr[j];
+            }
+        }
+        System.out.println(sum);
+    }
+
+}
